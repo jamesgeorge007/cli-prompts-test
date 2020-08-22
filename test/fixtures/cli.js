e63fe1e1fe18a3855e6cca70c25d17e92985f266
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-08-22 17:20:42 +0530

    chore: prettify test dir

--- test/fixtures/cli.js
+++ test/fixtures/cli.js
@@ -1,21 +1,15 @@
-const enquirer = require('enquirer');
+const enquirer = require("enquirer");
 
-const choices = [
-  'aqua',
-  'black',
-  'blue',
-  'fuchsia',
-  'green',
-  'gray',
-  'lime'
-];
+const choices = ["aqua", "black", "blue", "fuchsia", "green", "gray", "lime"];
 
-enquirer.prompt({
-  type: 'multiselect',
-  name: 'color',
-  message: 'Pick your favorite colors',
-  limit: 7,
-  choices,
-}).then(({color}) => {
-  console.log(`You chose ${color.join(', ')}`);
-});
+enquirer
+  .prompt({
+    type: "multiselect",
+    name: "color",
+    message: "Pick your favorite colors",
+    limit: 7,
+    choices,
+  })
+  .then(({ color }) => {
+    console.log(`You chose ${color.join(", ")}`);
+  });
--- test/test.js
+++ test/test.js
@@ -1,21 +1,27 @@
-'use strict';
+"use strict";
 
-const runTest = require('../');
+const runTest = require("../");
 
-const DOWN = '\x1B\x5B\x42';
-const ENTER = '\x0D';
-const SPACE = '\x20'
+const DOWN = "\x1B\x5B\x42";
+const ENTER = "\x0D";
+const SPACE = "\x20";
 
-describe('cli-prompts-test', () => {
+describe("cli-prompts-test", () => {
   const cliPath = `${__dirname}/fixtures/cli.js`;
 
-  it('picks a single option', async () => {
-  	const { stdout } = await runTest([cliPath], [`${DOWN}${DOWN}${SPACE}${ENTER}`]);
-    expect(stdout).toContain('You chose blue');
+  it("picks a single option", async () => {
+    const { stdout } = await runTest(
+      [cliPath],
+      [`${DOWN}${DOWN}${SPACE}${ENTER}`]
+    );
+    expect(stdout).toContain("You chose blue");
   });
 
-  it('picks multiple options', async () => {
-  	const { stdout } = await runTest([cliPath], [`${SPACE}${DOWN}${DOWN}${DOWN}${SPACE}${ENTER}${DOWN}${SPACE}${ENTER}`]);
-    expect(stdout).toContain('You chose aqua, fuchsia, green');
+  it("picks multiple options", async () => {
+    const { stdout } = await runTest(
+      [cliPath],
+      [`${SPACE}${DOWN}${DOWN}${DOWN}${SPACE}${ENTER}${DOWN}${SPACE}${ENTER}`]
+    );
+    expect(stdout).toContain("You chose aqua, fuchsia, green");
   });
 });
